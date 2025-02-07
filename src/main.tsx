07a8ecec1e99d24a354c616433e11d0d85a55ce4
AndBelkin <dev.andreubelkin@gmail.com>
2025-02-07 08:35:32 +0300

    create store & hooks (redux) for app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import NotebookApp from "./NotebookApp";
+import { Provider } from "react-redux";
+import store from "./app/store";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <NotebookApp />
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <NotebookApp />
+    </StrictMode>
+  </Provider>
 );
