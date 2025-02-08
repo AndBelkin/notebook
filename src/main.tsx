9df12c12ecd28df1a6bca95ff57bf2e2aaea10fb
AndBelkin <dev.andreubelkin@gmail.com>
2025-02-08 14:07:24 +0300

    add BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import NotebookApp from "./NotebookApp";
 import { Provider } from "react-redux";
 import store from "./app/store";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <StrictMode>
-      <NotebookApp />
-    </StrictMode>
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <StrictMode>
+        <NotebookApp />
+      </StrictMode>
+    </Provider>
+  </BrowserRouter>
 );
