2e444e39a11f621c3872a06570a1a9a60e145fe1
AndBelkin <dev.andreubelkin@gmail.com>
2025-02-10 17:12:35 +0300

    create DropDown

---
+++ src/components/drop-down/DropDown.tsx
@@ -0,0 +1,36 @@
+import { FC, useState } from "react";
+import { GrSort } from "react-icons/gr";
+import "./DropDown.css";
+
+interface DropDownProps {
+  items: string[];
+  getValue: (value: string) => void;
+}
+
+const DropDown: FC<DropDownProps> = ({ items, getValue }: DropDownProps) => {
+  const [mainItem, setMainItem] = useState<string>(items[0]);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const clickHandler = (item: string) => {
+    setMainItem(item);
+    setIsOpen(false);
+    getValue(item);
+  };
+  const dropBoxStyle = `drop-down-box ${isOpen ? "open" : ""}`;
+  const getList = items.filter((item) => item !== mainItem);
+  const renderList = getList.map((item, key) => (
+    <p key={key} onClick={() => clickHandler(item)}>
+      {item}
+    </p>
+  ));
+  return (
+    <div className={dropBoxStyle} onMouseLeave={() => setIsOpen(false)}>
+      <div className="drop-down-main-item" onClick={() => setIsOpen(!isOpen)}>
+        <p>{mainItem}</p>
+        <GrSort />
+      </div>
+      <div className="drop-down-list">{renderList}</div>
+    </div>
+  );
+};
+
+export default DropDown;
